1b63e0c18a18d6692ef2762c2ef1f60e5c627127
sen-guyen <kyurem82@gmail.com>
2023-06-26 09:25:42 -0700

    this is assignment 22

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
-
+    celsius = int(input('Enter a temperature in Celsius: '))
+    fahrenheit = ((9/5) * celsius) + 32
+    print (f'The equivalent temperature in fahrenheit is \t {fahrenheit:.2f}')
     """
     ########################################
     # Do not delete the return statement
